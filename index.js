2b645e0ad4a0c42b3dcf058ed2ce533237eb19a2
FernandoAndrada <fernandojcandrada@gmail.com>
2022-11-08 15:35:29 -0300

    Cria [M3S07] EX4 - Operadores

---
+++ index.js
@@ -0,0 +1,29 @@
+let x = 17;
+let y = 21;
+let z = 42;
+
+if(x < y && y < z){
+ console.log(`1.${Boolean=true}`)   
+}
+else {console.log(`1.${Boolean=false}`)}
+
+if(x != z || y > z){
+    console.log(`2.${Boolean=true}`)   
+   }
+   else {console.log(`2.${Boolean=false}`)}
+
+
+   if(z =(y*2)){
+    console.log(`3.${Boolean=true}`)   
+   }
+   else {console.log(`3.${Boolean=false}`)}
+
+   if((x + y) > z ){
+    console.log(`4.${Boolean=true}`)   
+   }
+   else {console.log(`4.${Boolean=false}`)}
+
+   if(((x*x)+(y*y)) > (z*z)){
+    console.log(`5.${Boolean=true}`)   
+   }
+   else {console.log(`5.${Boolean=false}`)}
